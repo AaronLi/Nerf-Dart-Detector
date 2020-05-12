5efeb6a84ba97219987c944b48f113c824cf64c8
Daniel Duan <danielduan01@gmail.com>
2020-05-11 23:36:16 -0400

    Added comparison for edge-detection lines and lines between tips and bodies. Uses difference between the polar coordinates of the two to predict which tips belong to which bodies.

--- main.py
+++ main.py
@@ -76,8 +76,8 @@ def find_darts(image, show_stages=False):
     body_points = dart_body_detector.detect(detect_bodies)
     detect_tips = cv2.blur(cv2.cvtColor(cv2.cvtColor(dart_tips, cv2.COLOR_HSV2BGR), cv2.COLOR_BGR2GRAY), (4, 4))
     tip_points = dart_tip_detector.detect(detect_tips)
-    body_edges = cv2.Canny(detect_bodies, 150, 200, apertureSize=5)
-
+    # body_edges = cv2.Canny(detect_bodies, 150, 200, apertureSize=5)
+    '''
     lines = cv2.HoughLines(body_edges, 1, np.pi / 360, 50)
     if lines is not None:
         for line in lines:
@@ -95,7 +95,7 @@ def find_darts(image, show_stages=False):
         lines = [line[0] for line in lines]
         lines.sort(key=lambda x: x[1])
         # print(len(lines), lines)
-
+    '''
     for tip in tip_points:
         cv2.circle(image, (int(tip.pt[0]), int(tip.pt[1])), int(tip.size), (0, 255, 255), thickness=2)
 
@@ -105,7 +105,7 @@ def find_darts(image, show_stages=False):
 
     for dart in body_points:
         for tip in tip_points:
-            rho, theta = polar.get_polar(dart.pt, tip.pt)
+            '''rho, theta = polar.get_polar(dart.pt, tip.pt)
             a = np.cos(theta)
             b = np.sin(theta)
             x0 = a * rho
@@ -121,41 +121,48 @@ def find_darts(image, show_stages=False):
                 darty = dartiness(line, rho, theta)
                 all_combos.append([darty, line, tip, dart])
 
-            '''
-            max_index = np.argmax(potential_darts[:, 0])
+            
+            #max_index = np.argmax(potential_darts[:, 0])
 
             # instead of swapping out largest and smallest values just append them to a list and use heapsort
             if darty[0] < potential_darts[max_index, 0]:
                 potential_darts[max_index] = [darty[0], lines[darty[1]], tip, dart]
             # print(potential_darts)'''
+            distance = np.hypot((dart.pt[0]-tip.pt[0]), (dart.pt[1]-tip.pt[1]))
+            #if distance in set(all_combos):
+                #distance += 0.00001
+
+            all_combos.append([distance, tip, dart])
 
         cv2.circle(image, (int(dart.pt[0]), int(dart.pt[1])), int(dart.size * 2), (90, 255, 255), thickness=2)
 
 
     all_combos.sort()
     for dart in all_combos:
-        #print('Combo Dart', polar.get_polar(dart[3].pt, dart[2].pt))
-        #print('Line:', dart[1])
-        print('Dartiness', dart[0], '\n')
-        if not dart[2] in used_combos and not dart[3] in used_combos and len(potential_darts) < min(len(tip_points), len(body_points)):
+        # print('Combo Dart', polar.get_polar(dart[3].pt, dart[2].pt))
+        # print('Line:', dart[1])
+        print('Distance', dart[0], '\n')
+        if not dart[1] in used_combos and not dart[2] in used_combos and len(potential_darts) < min(len(tip_points),
+                                                                                                    len(body_points)):
             potential_darts.append(dart)
+            used_combos.append(dart[1])
             used_combos.append(dart[2])
-            used_combos.append(dart[3])
     '''print(potential_darts[0, 3].pt)'''
     for potent in potential_darts:
         # print(int(potent[3].pt[0]))
-        tipa = int(potent[2].pt[0])
-        tipb = int(potent[2].pt[1])
-        bodya = int(potent[3].pt[0])
-        bodyb = int(potent[3].pt[1])
+        tipa = int(potent[1].pt[0])
+        tipb = int(potent[1].pt[1])
+        bodya = int(potent[2].pt[0])
+        bodyb = int(potent[2].pt[1])
         bodypoint = (bodya, bodyb)
         tippoint = (tipa, tipb)
-        cv2.putText(image, str(int(potent[0])), bodypoint, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-        cv2.putText(image, str(int(potent[0])), tippoint, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        print(potent[0])
+        cv2.putText(image, str(int(potent[0])), bodypoint, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(image, str(int(potent[0])), tippoint, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
 
-        print('Potential Dart',polar.get_polar(potent[3].pt,potent[2].pt))
-        print('Line:',potent[1])
-        print('Dartiness',potent[0],'\n')
+        print('Potential Dart', polar.get_polar(potent[2].pt, potent[1].pt))
+        # print('Line:',potent[1])
+        print('Distance', potent[0], '\n')
 
     show_stages = True
     if show_stages:
@@ -165,8 +172,8 @@ def find_darts(image, show_stages=False):
         plt.show()
         plt.imshow(cv2.cvtColor(dart_tips, cv2.COLOR_HSV2RGB))
         plt.show()
-        plt.imshow(cv2.cvtColor(body_edges, cv2.COLOR_GRAY2RGB))
-        plt.show()
+        #plt.imshow(cv2.cvtColor(body_edges, cv2.COLOR_GRAY2RGB))
+        #plt.show()
     return image
 
 
@@ -177,11 +184,10 @@ def dartiness(line, dart_rho, dart_theta):
         if darty < temp[0]:
             temp[0] = darty
             temp[1] = i'''
-    darty = (line[0] % 180 - dart_theta% 180 ) ** 2 + (line[1] - dart_rho) ** 2
+    darty = (line[0] % 180 - dart_theta % 180) ** 2 + (line[1] - dart_rho) ** 2
     return darty
 
-
-# #plt.imshow(cv2.cvtColor(threshold[1], cv2.COLOR_GRAY2RGB))
+# #I plt.imshow(cv2.cvtColor(threshold[1], cv2.COLOR_GRAY2RGB))
 # #cv2.imshow("pic", cv2.cvtColor(filter_image, cv2.COLOR_HSV2BGR))
 # #while True:
 # #    if cv2.waitKey(1) == 113:
